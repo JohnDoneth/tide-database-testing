3704b8dd3380182715aad2b2e0b1855aa770aef2
John C. Doneth <doneth7@gmail.com>
2019-08-02 13:48:34 -0400

    added async logging

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
 #![feature(async_await)]
-#![deny(warnings)]
+//#![deny(warnings)]
 #![feature(duration_float)]
 
 use tokio::net::TcpStream;
 use tokio::sync::lock::{Lock, LockGuard};
 
 use tokio_postgres::tls::{NoTls, NoTlsStream};
-use tokio_postgres::{Statement, Client, Config, Connection, Error};
+use tokio_postgres::{Client, Config, Connection, Error, Statement};
 
-use std::pin::Pin;
 use std::collections::HashMap;
+use std::pin::Pin;
 
 use futures::{Future, FutureExt, Poll, StreamExt};
 
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
+
+use uuid::Uuid;
+
+#[macro_use(
+    o,
+    slog_o,
+    slog_info,
+    slog_log,
+    slog_record,
+    slog_record_static,
+    slog_b,
+    slog_kv
+)]
+extern crate slog;
+
+#[macro_use]
+extern crate slog_scope;
+
+extern crate slog_async;
+extern crate slog_term;
+
+use slog::Drain;
 
 async fn connect_raw(s: &str) -> Result<(Client, Connection<TcpStream, NoTlsStream>), Error> {
     let socket = TcpStream::connect(&"127.0.0.1:5433".parse().unwrap())
@@ -74,8 +96,6 @@ impl Pool {
             }));
         }
 
-        println!("opened {} database connections", num);
-
         Self(SharedPool { connections })
     }
 
@@ -103,9 +123,16 @@ impl StatementCache {
     }
 }
 
+async fn html(_: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    Ok(Response::new(Body::from("Hello World!")))
+}
+
+use slog::Logger;
+
 async fn hello(
     _: Request<Body>,
     connection: GetConnectionFuture,
+    log: Logger,
 ) -> Result<Response<Body>, hyper::Error> {
     let mut result = {
         let mut connection = connection.await;
@@ -131,35 +158,52 @@ async fn hello(
     let res = if let Some(Ok(next)) = result.next().await {
         next.get::<_, &str>(0).to_string()
     } else {
-        "failed to query".to_string()
+        slog_crit!(log, "failed to execute query");
+        "internal server error".to_string()
     };
+
+    slog_info!(log, "request complete");
+
     Ok(Response::new(Body::from(res)))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    pretty_env_logger::init();
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).chan_size(1000).build().fuse();
+
+    let log = slog::Logger::root(drain, o!());
+    let _guard = slog_scope::set_global_logger(log.clone());
+
+    info!("Logging ready!");
 
     let pool = Pool::new(8).await;
 
+    info!("Database connections established"; "count" => 8);
+
     let addr = "127.0.0.1:8000".parse().unwrap();
 
-    let server = Server::bind(&addr).serve(make_service_fn(move |_| {
+    let server = Server::bind(&addr).serve(make_service_fn(|_| {
         let pool = pool.clone();
+
+        let log = log.clone();
 
         // This is the `Service` that will handle the connection.
         // `service_fn` is a helper to convert a function that
         // returns a Response into a `Service`.
         async {
             Ok::<_, hyper::Error>(service_fn(move |req| {
+                let request_logger = log.new(o!("request" => Uuid::new_v4().to_string()));
+
                 let connection = pool.get();
 
-                hello(req, connection)
+                hello(req, connection, request_logger)
             }))
         }
     }));
 
-    println!("Listening on http://{}", addr);
+    info!("Listening on http://{}", addr);
 
     server.await?;
 
